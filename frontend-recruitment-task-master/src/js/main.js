ef68b5a913711a5311ba9c2eb4c1a1adfe455ab5
jandutkiewicz0309 <jan.dutkiewicz@op.pl>
2022-06-06 14:02:29 +0200

    the third is commit functions

--- frontend-recruitment-task-master/src/js/main.js
+++ frontend-recruitment-task-master/src/js/main.js
@@ -1,8 +1,26 @@
-document.getElementById('button').addEventListener('click',
-function(){
-document.querySelector('.alert-modal').style.display = 'flex'
-});
 
-document.querySelector('.close').addEventListener("click", function() {
-	document.querySelector('.alert-modal-close'). style.display = "none"
-});
+
+
+
+
+// document.getElementById('button').addEventListener('click',
+
+
+
+
+
+// function(){
+// document.querySelector('.alert-modal').style.display = 'flex'
+// });
+
+// document.querySelector('.close').addEventListener("click", function() {
+// 	document.querySelector('.alert-modal-close'). style.display = "none"
+// });
+
+{/* <script type="text/javascript">
+var clicks = 0;
+function onClick() {
+clicks += 1;
+document.getElementById("clicks").innerHTML = clicks;
+};
+</script> */}
